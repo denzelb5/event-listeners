4dd3bcc4c38ee0a19cbfe7fde5ced3b115b2ddfd
Denise Baker <denviol@yahoo.com>
2019-08-27 21:36:37 -0500

    event listeners

--- main.js
+++ main.js
@@ -1,56 +1,96 @@
 console.log('hi');
 
 const printToDom = (toPrint, divId) => {
-    document.getElementById(divId).innerHTML += toPrint;
+    document.getElementById(divId).innerHTML = toPrint;
 }
 
-let pieList = [{
+let pies = [{
     name: 'pumpkin',
     image: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Pumpkin-Pie-Slice.jpg',
-    instructor: 'Zoe'
+    instructor: 'zoeee'
 },
 {
     name: 'cherry',
     image: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Cherry-Pie-Slice.jpg',
-    instructor: 'Michael'
+    instructor: 'michael'
 },
 {
     name: 'key lime',
     image: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Keylimepiecut.jpg',
-    instructor: 'Callan'
+    instructor: 'callan'
 },
 {
     name: 'peach',
     image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Peach_pie_%286043640689%29.jpg',
-    instructor: 'Zoe'
+    instructor: 'zoeee'
 },
 {
     name: 'apple',
     image: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Apple_pie.jpg',
-    instructor: 'Callan'
+    instructor: 'callan'
 },
 {
     name: 'chocolate',
     image: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Sliced_black_walnut_and_chocolate_pie_%285772382054%29.jpg',
-    instructor: 'Greg'
+    instructor: 'greg'
 }]
 
 const pieBuilder = (pieArr) => {
+    let domString = ''
     for (let i = 0; i < pieArr.length; i++) {
         const pies = pieArr[i];
-        const domString = `
+        domString += `
         <div class="card">
         <h2>${pies.name}</h2>
         <img src='${pies.image}' alt='Image of ${pies.name}'>
-        <h4>${pies.instructor}</h4>
+        
         </div>
         `
-    printToDom(domString, 'pie-zone')
+    
     }
+    printToDom(domString, 'pie-zone')
 }
 
-pieBuilder(pieList)
+
 
 //document.getElementById('my-button').addEventListener('click', ()=> {
- //   console.log('bike')
+  //  console.log('bike')
 //})
+
+ 
+    //figure out who this instructor is thr the button we clicked on
+    const buttonClick = (e) => {
+    const instructor = event.target.id
+    
+    // only get those pies from the list of all the pies
+    const selectedPies = []
+    for (let i = 0; i < pies.length; i++) {
+        const pie = pies[i]
+        if (pie.instructor === instructor) {
+            selectedPies.push(pie)
+        }
+    }
+    // pass small list of pies back into the pie builder function
+    pieBuilder(selectedPies)
+}
+document.getElementById('zoeee').addEventListener('click', buttonClick)
+
+
+document.getElementById('michael').addEventListener('click', (event) => {
+    //figure out who this instructor is thr the button we clicked on
+    const instructor = event.target.id
+    
+    // only get those pies from the list of all the pies
+    const selectedPies = []
+    for (let i = 0; i < pies.length; i++) {
+        const pie = pies[i]
+        if (pie.instructor === instructor) {
+            selectedPies.push(pie)
+        }
+    }
+    // pass small list of pies back into the pie builder function
+    pieBuilder(selectedPies)
+})
+
+//pieBuilder(pies)
+
